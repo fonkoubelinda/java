1a37b397258e40dd38b762800992ea9e2c5da081
fonkoubelinda <belind.dev@gmail.com>
2023-07-26 08:47:59 +0200

    java

---
+++ init.js/script.js
@@ -0,0 +1,72 @@
+// let moy =prompt("entrer votre moyenne");
+// if (moy>10)
+// {
+//     alert("admis");
+// }
+// else(moy<10)
+// {
+//     alert("echec");
+// }
+
+
+// let somme=0;
+// let nombre1=0;
+// let nombre2=0;
+//  nombre1=parseInt(prompt ("entrer votre 1er nombre") );
+//  nombre2=parseInt(prompt ("entrer votre 2nd nombre") );
+// somme=nombre1+nombre2;
+// alert(` la somme est ${nombre1} + ${nombre2} est: ${somme}`);
+
+
+// let sub=0;
+// let nombre=0;
+// let nombre2=0;
+// nombre1=prompt("entrer 1er nombre");
+// nombre2=prompt("entrer 2nd nombre");
+// sub=nombre1-nombre2;
+// alert(`la subtraction est ${nombre1}-${nombre2} est: ${sub}`);
+
+// let prod=0;
+// let nombre1=0;
+// let nombre2=0;
+// nombre1=prompt("entrer 1er nombre");
+// nombre2=prompt("entrer 2nd nombre");
+// prod=nombre1*nombre2;
+// alert(`le produit est ${nombre1} * ${nombre2} est: ${prod}`)
+
+// let div=0;
+// let nombre1=0;
+// let nombre2=0;
+// nombre1=prompt("entrer 1er nombre ");
+// nombre2=prompt("entrer 2nd nombre ");
+// div=nombre1/nombre2;
+// alert(`la division est ${nombre1} / ${nombre2} est: ${div}`);
+
+// let carr=0;
+// let a=0;
+// a=prompt("entrer un nombre");
+// carr=Math.sqrt(a);
+// alert(`le carré est ${a} est: ${carr} `)
+
+
+// let a=0;
+// let b=0;
+// let c=1;
+// a=prompt("entrer nombre >0 ");
+// if (a>0)
+// {
+//     alert(` positive number is ${a} est: +${a}`);
+// }
+// else(a<0)
+// {
+//     alert(`positive number is est: +${a}`);
+// }
+
+
+let a=0;
+let value=0;
+a=prompt("entrer nombre");
+value=Math.abs(a);
+alert(`la value de  ${a} est: +${value}`);
+
+
